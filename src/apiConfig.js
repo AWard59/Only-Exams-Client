394b3e72c8cb021e993e9a2abf2f283249342730
AWard59 <iamsancho5990@hotmail.com>
2022-03-02 16:02:13 -0800

    initial commit

---
+++ src/apiConfig.js
@@ -0,0 +1,12 @@
+let apiUrl
+const apiUrls = {
+  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  development: 'http://localhost:8000'
+}
+if (window.location.hostname === 'localhost') {
+  apiUrl = apiUrls.development
+} else {
+  apiUrl = apiUrls.production
+}
+
+export default apiUrl
